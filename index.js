209a30342049ed04cc51515da94319b9a1a76f72
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2022-04-23 17:36:19 -0400

    fixed

--- index.js
+++ index.js
@@ -29,18 +29,24 @@ module.exports = class timestamp extends Plugin {
       const regexA = new RegExp(`${char.length ? (`${ char}`) : ''}((?<!\\d)\\d{1,2}:\\d{2}(?!\\d))(am|pm)?`, 'i');
       if (args[1].content.search(regexAGlobal) !== -1) args[1].content = args[1].content.replace(regexAGlobal, x => {
         let [, time, suffix] = x.match(regexA);
-        if (suffix && suffix.toLowerCase() === 'pm') {
-          let [hours, minutes] = time.split(':').map(e => parseInt(e));
+        let [hours, minutes] = time.split(':').map(e => parseInt(e));
+        if (suffix && suffix.toLowerCase() === 'pm' && hours > 12 && hours === 0) {
           hours += 12;
+          minutes = minutes.toString().padStart(2, '0');
+          time = `${hours}:${minutes}`;
+        } else if ((suffix && suffix.toLowerCase() === 'am' && hours === 12) || (hours === 24)) time = `00:${minutes}`;
+        else if (minutes >= 60) {
+          hours += Math.floor(minutes / 60);
+          minutes = (minutes % 60);
           time = `${hours}:${minutes}`;
         }
         return this.getUnixTimestamp(time);
       });
-      
       return args;
     }, true);
   }
   getUnixTimestamp(time) {
+    console.log(time);
     const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d?\d:\d\d/, time);
     const then = Math.round((new Date(date)).getTime() / 1000);
     if (isNaN(then)) return time;
