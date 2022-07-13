6ac12c6a204a1b752e77ac0d839ac907e163d6fd
thakyZ <thakyz@outlook.com>
2022-07-13 13:53:54 -0700

    Added ability to supply own format with custom delimiter

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const { Plugin } = require('powercord/entities');
-const { inject, uninject } = require('powercord/injector');
-const { messages } = require('powercord/webpack');
+const { Plugin } = require("powercord/entities");
+const { inject, uninject } = require("powercord/injector");
+const { messages } = require("powercord/webpack");
 
 let prefix;
-const Settings = require('./Components/settings.jsx');
+let suppliedPrefix;
+const Settings = require("./Components/settings.jsx");
 module.exports = class timestamp extends Plugin {
   startPlugin() {
-    this.setDefault('t', true);
+    this.setDefault("t", true);
     powercord.api.settings.registerSettings(this.entityID, {
       category: this.entityID,
-      label: 'Replace Timestamps',
-      render: Settings
+      label: "Replace Timestamps",
+      render: Settings,
     });
     this.patchMessage();
   }
@@ -18,41 +19,104 @@ module.exports = class timestamp extends Plugin {
     this.settings.set(name, this.settings.get(name, defaultValue));
     prefix = name;
   }
-  patchMessage() { //Lighty made this better because he felt like it
-    inject('timestamp', messages, 'sendMessage', args => {
-      
-      let char = this.settings.get('char');
-      if (!char) char = '';
-      char.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'); 
-      const regexAGlobal = new RegExp(`${char.length ? (`${ char}`) : ''}(?<!\\d)\\d{1,2}:\\d{2}(?!\\d)(am|pm)?`, 'gi');
-      const regexA = new RegExp(`${char.length ? (`${ char}`) : ''}((?<!\\d)\\d{1,2}:\\d{2}(?!\\d))(am|pm)?`, 'i');
-      if (args[1].content.search(regexAGlobal) !== -1) args[1].content = args[1].content.replace(regexAGlobal, x => {
-        let [, time, suffix] = x.match(regexA);
-        let [hours, minutes] = time.split(':').map(e => parseInt(e));
-        if (suffix && suffix.toLowerCase() === 'pm' && hours < 12 && hours !== 0) {
-          hours += 12;
-          minutes = minutes.toString().padStart(2, '0');
-          time = `${hours}:${minutes}`;
-        } else if ((suffix && suffix.toLowerCase() === 'am' && hours === 12) || (hours === 24)) time = `00:${minutes}`;
-        else if (minutes >= 60) {
-          hours += Math.floor(minutes / 60);
-          minutes = (minutes % 60);
-          time = `${hours}:${minutes}`;
+  patchMessage() {
+    //Lighty made this better because he felt like it
+    inject(
+      "timestamp",
+      messages,
+      "sendMessage",
+      (args) => {
+        let char = this.settings.get("char");
+        if (!char) char = "";
+        char.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+        console.log(
+          args[1].content
+            .replace(`${char.length ? `${char}` : ""}`, "")
+            .substring(1, 2)
+        );
+        if (
+          char.length > 0 &&
+          args[1].content
+            .replace(`${char.length ? `${char}` : ""}`, "")
+            .substring(1, 2) === char[char.length - 1] &&
+          args[1].content
+            .replace(`${char.length ? `${char}` : ""}`, "")
+            .substring(1, 2)
+            .match(/\W/g) &&
+          char[char.length - 1].match(/\W/g)
+        ) {
+          suppliedPrefix = args[1].content
+            .replace(`${char.length ? `${char}` : ""}`, "")
+            .substring(0, 1);
+          char = `${char}${suppliedPrefix}${char[char.length - 1]}`;
         }
-        return this.getUnixTimestamp(time);
-      });
-      return args;
-    }, true);
+        const regexAGlobal = new RegExp(
+          `${
+            char.length ? `${char}` : ""
+          }(?<!\\d)\\d{1,2}:\\d{2}(?!\\d)(am|pm)?`,
+          "gi"
+        );
+        const regexA = new RegExp(
+          `${
+            char.length ? `${char}` : ""
+          }((?<!\\d)\\d{1,2}:\\d{2}(?!\\d))(am|pm)?`,
+          "i"
+        );
+        if (args[1].content.search(regexAGlobal) !== -1)
+          args[1].content = args[1].content.replace(regexAGlobal, (x) => {
+            let [, time, suffix] = x.match(regexA);
+            let [hours, minutes] = time.split(":").map((e) => parseInt(e));
+            if (
+              suffix &&
+              suffix.toLowerCase() === "pm" &&
+              hours < 12 &&
+              hours !== 0
+            ) {
+              hours += 12;
+              minutes = minutes.toString().padStart(2, "0");
+              time = `${hours}:${minutes}`;
+            } else if (
+              (suffix && suffix.toLowerCase() === "am" && hours === 12) ||
+              hours === 24
+            )
+              time = `00:${minutes}`;
+            else if (minutes >= 60) {
+              hours += Math.floor(minutes / 60);
+              minutes = minutes % 60;
+              time = `${hours}:${minutes}`;
+            }
+            return this.getUnixTimestamp(time);
+          });
+        return args;
+      },
+      true
+    );
   }
   getUnixTimestamp(time) {
-    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').replace(/\d?\d:\d\d/, time);
-    const then = Math.round((new Date(date)).getTime() / 1000);
+    const date = new Date()
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\..+/, "")
+      .replace(/\d?\d:\d\d/, time);
+    const then = Math.round(new Date(date).getTime() / 1000);
     if (isNaN(then)) return time;
-    if (this.settings.get('t')) prefix = 't'; else if (this.settings.get('T')) prefix = 'T'; else if (this.settings.get('d')) prefix = 'd'; else if (this.settings.get('D')) prefix = 'D'; else if (this.settings.get('f')) prefix = 'f'; else if (this.settings.get('F')) prefix = 'F'; else if (this.settings.get('R')) prefix = 'R';
+    console.log("prefix:" + suppliedPrefix);
+    if (!suppliedPrefix) {
+      if (this.settings.get("t")) prefix = "t";
+      else if (this.settings.get("T")) prefix = "T";
+      else if (this.settings.get("d")) prefix = "d";
+      else if (this.settings.get("D")) prefix = "D";
+      else if (this.settings.get("f")) prefix = "f";
+      else if (this.settings.get("F")) prefix = "F";
+      else if (this.settings.get("R")) prefix = "R";
+    } else {
+      prefix = suppliedPrefix;
+    }
     return `<t:${then}:${prefix}>`;
   }
   pluginWillUnload() {
-    uninject('timestamp'); powercord.api.settings.unregisterSettings(this.entityID);
+    uninject("timestamp");
+    powercord.api.settings.unregisterSettings(this.entityID);
     powercord.api.settings.unregisterSettings(this.entityID);
   }
 };
